afa223a39258cd8f665da5dd3d90a9d9ee476efd
Rully Varelio <125984874+MindfullRV@users.noreply.github.com>
2023-08-25 09:55:47 +0700

    make bot mirror user message

--- main.py
+++ main.py
@@ -3,12 +3,36 @@ import os
 import slack
 from pathlib import Path
 from dotenv import load_dotenv
+from flask import Flask
+from slackeventsapi import SlackEventAdapter
 
 # setting the environment variable path using dotenv
 environment_variable_path = Path(".") / ".env"
 load_dotenv(dotenv_path=environment_variable_path)
 
+# flask configuration with slack api
+app = Flask(__name__)
+slack_event_adapter = SlackEventAdapter(
+    os.environ["SIGNING_SECRET"], "/slack/events", app
+)
+
 # connect slack with python code
 client = slack.WebClient(token=os.environ["SLACK_TOKEN"])
+BOT_ID = client.api_call("auth.test")["user_id"]  # get bot id
 
-client.chat_postMessage(channel="#youtube_bot", text="Hello, world!")
+
+# functions for slack events
+@slack_event_adapter.on("message")
+def message(payload):
+    event = payload.get("event", {})
+    channel_id = event.get("channel")  # get channel id the bot are in
+    user_id = event.get("user")  # get user id the user that sent prompt
+    text = event.get("text")  # just text
+
+    # send a mirror message to user
+    if BOT_ID != user_id:  # to prevent messages from looping
+        client.chat_postMessage(channel=channel_id, text=text)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
